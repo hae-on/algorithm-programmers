5e6a1a35e0775344c5d848f8ca47291d4968abcc
hae-on <solbi2004@naver.com>
2021-05-22 22:28:30 +0900

    [Add] 문자열을 정수로 바꾸기, 최대공약수와 최소공배수

---
+++ Level_1/문자열을정수로바꾸기.js
@@ -0,0 +1,11 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12925
+
+function solution(s) {
+  const answer = Number(s);
+  return answer;
+}
+
+// 다른풀이
+function solution(s) {
+  return s / 1;
+}
---
+++ Level_1/최대공약수와최소공배수.js
@@ -0,0 +1,8 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12940
+
+function solution(n, m) {
+  // 유클리드 호제법: a를 b로 나눈 나머지를 r이라 하면 a와 b의 최대공약수는 b와 r의 최대공약수와 같다.
+  const gcd = (a, b) => (a % b === 0 ? b : gcd(b, a % b)); //최대공약수
+  const lcm = (a, b) => (a * b) / gcd(a, b); //최소공배수
+  return [gcd(n, m), lcm(n, m)]; //배열값으로 반환
+}
